5f760219a84359761823c034b17858b3505b75ed
narendran <narendrandlk@gmail.com>
2025-05-26 00:40:10 +0530

    coding challenges updated

--- Coding_Challenges/SQL/WeatherObservation.sql
+++ Coding_Challenges/SQL/WeatherObservation.sql
@@ -1,10 +1,30 @@
-(select city,LENGTH(city) from station 
+/*
+
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths 
+(i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+
+*/
+select city,LENGTH(city) from station 
 where length(city) = (select min(length(city)) from station)
 order by city asc 
-limit 1)
-union
-(select city,LENGTH(city) from station 
+fetch first 1 Row only;
+select city, LENGTH(city) from station 
 where length(city) = (select max(length(city)) from station)
 order by city asc 
-limit 1
-)
+fetch first 1 Row only;
+
+
+/*
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+*/
+
+select distinct city from station
+where lower(substr(city,1,1)) in ('a','e','i','o','u');
+
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+*/
+
+select distinct city from station
+where lower(substr(city,length(city),1)) in ('a','e','i','o','u');
